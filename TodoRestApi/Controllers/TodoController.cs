03088d4ece1514cdbcf24ec21783e054d8e1ca44
andrenaMatthiasZ <matthias.zeleny@andrena.de>
2017-09-18 14:44:32 +0200

    Add TodoController

--- TodoRestApi/Controllers/TodoController.cs
+++ TodoRestApi/Controllers/TodoController.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TodoRestApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TodoRestApi.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TodoController : Controller
     {
-        // GET api/values
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext todoContext)
+        { _context = todoContext;
+            if(_context.TodoItems.Count()==0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+
+            }
+        }
+
+        // GET: api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
--- TodoRestApi/Models/TodoItem.cs
+++ TodoRestApi/Models/TodoItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace TodoRestApi.Models
+﻿namespace TodoRestApi.Models
 {
     public class TodoItem
     {
